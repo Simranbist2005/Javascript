7681cd9186235c9cf950334141162d7e185e4246
Simranbist2005 <seemranbist@gmail.com>
2026-01-07 12:40:32 +0000

    New update

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 206227;
+let accountEmail = "seemranbist@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mahendranagar";
+
+let accountState;
+//accountId = 2;
+
+accountEmail = "hcemail.com"
+accountPassword = "212121"
+accountCity = "Kathmandu"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
