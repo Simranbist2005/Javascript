a039bb16799690223d3af8a621a9a0ec6d97e3f4
Simranbist2005 <seemranbist@gmail.com>
2026-01-07 15:38:04 +0000

    New changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,27 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********************* Operations *******************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2**3);
+
+// let str1 = "Simran"
+// let str2 = "Bista"
+// let str3 = str1 + str2
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+++gameCounter; //prefix
+gameCounter++; //postfix
+console.log(gameCounter)
